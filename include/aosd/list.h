a047de18c6151e57054f0429e4a102b7ef5296f4
pzzzyda <zhongyuan03@outlook.com>
2026-02-11 14:36:31 +0800

    refactor: refactor list.h
    Renamed some functions and added some iteration macros.

--- include/aosd/list.h
+++ include/aosd/list.h
@@ -4,94 +4,102 @@
 #include <aosd/macros.h>
 #include <aosd/types.h>
 
-#define LIST_HEAD_INIT(name) { &name, &name }
+#define LIST_HEAD_INITIALIZER(name) { &name, &name }
+#define LIST_HEAD(name) struct list_head name = LIST_HEAD_INITIALIZER(name)
 
-static inline void __list_add(struct list_head *new, struct list_head *prev,
+static inline void __list_add(struct list_head *node, struct list_head *prev,
 			      struct list_head *next)
 {
-	next->prev = new;
-	new->next = next;
-	new->prev = prev;
-	prev->next = new;
+	next->prev = node;
+	node->next = next;
+	node->prev = prev;
+	prev->next = node;
 }
 
-static inline void __list_del(struct list_head *entry)
+static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
-	entry->prev->next = entry->next;
-	entry->next->prev = entry->prev;
-	entry->next = entry;
-	entry->prev = entry;
+	next->prev = prev;
+	prev->next = next;
 }
 
-static inline bool list_is_empty(struct list_head *list)
+static inline bool list_empty(struct list_head *head)
 {
-	return list->next == list;
+	return head->next == head;
 }
 
-static inline struct list_head *list_get_first(struct list_head *list)
+static inline void list_init_head(struct list_head *head)
 {
-	return list->next;
+	head->next = head;
+	head->prev = head;
 }
 
-static inline struct list_head *list_get_last(struct list_head *list)
+static inline void list_add(struct list_head *node, struct list_head *head)
 {
-	return list->prev;
+	__list_add(node, head, head->next);
 }
 
-#define list_for_each(curr, list) \
-	for (curr = list_get_first(list); curr != list; curr = curr->next)
-
-#define list_for_each_rev(curr, list) \
-	for (curr = list_get_last(list); curr != list; curr = curr->prev)
-
-static inline void list_init(struct list_head *list)
+static inline void list_add_tail(struct list_head *node, struct list_head *head)
 {
-	list->next = list;
-	list->prev = list;
+	__list_add(node, head->prev, head);
 }
 
-static inline void list_add_first(struct list_head *list, struct list_head *new)
+static inline void list_del(struct list_head *node)
 {
-	__list_add(new, list, list->next);
+	__list_del(node->prev, node->next);
+	node->next = NULL;
+	node->prev = NULL;
 }
 
-static inline void list_add_last(struct list_head *list, struct list_head *new)
-{
-	__list_add(new, list->prev, list);
-}
+#define list_for_each(curr, head) \
+	for (curr = (head)->next; curr != (head); curr = curr->next)
 
-static inline void list_del(struct list_head *entry)
-{
-	__list_del(entry);
-}
+#define list_for_each_reverse(curr, head) \
+	for (curr = (head)->prev; curr != (head); curr = curr->prev)
 
-static inline void list_del_first(struct list_head *list)
-{
-	if (!list_is_empty(list))
-		__list_del(list_get_first(list));
-}
+#define list_for_each_safe(curr, next, head)                         \
+	for (curr = (head)->next, next = curr->next; curr != (head); \
+	     curr = next, next = next->next)
 
-static inline void list_del_last(struct list_head *list)
-{
-	if (!list_is_empty(list))
-		__list_del(list_get_last(list));
-}
+#define list_for_each_reverse_safe(curr, next, head)                 \
+	for (curr = (head)->prev, next = curr->prev; curr != (head); \
+	     curr = next, next = next->prev)
 
-static inline size_t list_count(struct list_head *list)
-{
-	size_t count = 0;
-	struct list_head *curr;
-	list_for_each(curr, list)
-		++count;
-	return count;
-}
+#define list_entry(ptr, type, member) container_of(ptr, type, member)
 
-#define list_entry(list, type, member) container_of(list, type, member)
+#define list_first_entry(head, type, member) \
+	list_entry((head)->next, type, member)
 
-#define list_first_entry(list, type, member) \
-	list_entry(list_get_first(list), type, member)
+#define list_last_entry(head, type, member) \
+	list_entry((head)->prev, type, member)
 
-#define list_last_entry(list, type, member) \
-	list_entry(list_get_last(list), type, member)
+#define list_next_entry(curr, member) \
+	list_entry((curr)->member.next, typeof(*(curr)), member)
+
+#define list_prev_entry(curr, member) \
+	list_entry((curr)->member.prev, typeof(*(curr)), member)
+
+#define list_entry_is_head(curr, head, member) (&(curr)->member == (head))
+
+#define list_for_each_entry(curr, head, member)                    \
+	for (curr = list_first_entry(head, typeof(*curr), member); \
+	     !list_entry_is_head(curr, head, member);              \
+	     curr = list_next_entry(curr, member))
+
+#define list_for_each_entry_reverse(curr, head, member)           \
+	for (curr = list_last_entry(head, typeof(*curr), member); \
+	     !list_entry_is_head(curr, head, member);             \
+	     curr = list_prev_entry(curr, member))
+
+#define list_for_each_entry_safe(curr, next, head, member)         \
+	for (curr = list_first_entry(head, typeof(*curr), member), \
+	    next = list_next_entry(curr, member);                  \
+	     !list_entry_is_head(curr, head, member);              \
+	     curr = next, next = list_next_entry(next, member))
+
+#define list_for_each_entry_reverse_safe(curr, next, head, member) \
+	for (curr = list_last_entry(head, typeof(*curr), member),  \
+	    next = list_prev_entry(curr, member);                  \
+	     !list_entry_is_head(curr, head, member);              \
+	     curr = next, next = list_prev_entry(next, member))
 
 #endif
